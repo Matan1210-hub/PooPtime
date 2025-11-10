cecfd872486f5909c619cab78d04d73050265c2e
matan <matanbcohen@gmail.com>
2025-11-10 20:32:16 +0200

    Initial Commit

---
+++ PooPtime/PooPtimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PooPtimeApp.swift
+//  PooPtime
+//
+//  Created by Matan Cohen on 10/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct PooPtimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
